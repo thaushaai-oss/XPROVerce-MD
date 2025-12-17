80492c4f10fe71a060e8f37a831a82090a57bb0a
thaushaai-oss <thaushaai@gmail.com>
2025-12-17 17:09:01 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPROVerce~qJLr5wl4QF",
 PORT: process.env.PORT || "8000"
 };
